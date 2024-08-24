8390724463cf9e3f17d9ab4cb130b71448fbab98
Bluscream <github@minopia.de>
2024-08-24 21:33:15 +0200

    use ctx in cache

--- ohd/Scripts/hooks/player.lua
+++ ohd/Scripts/hooks/player.lua
@@ -32,25 +32,55 @@ PlayerHooks.Init = function (Player)
     Utils.Log("Initialized hooks for Player: %s", Player)
 end
 
-PlayerHooks.Methods.onReceiveNewChatMsg = function (self) 
+PlayerHooks.Methods.onReceiveNewChatMsg = function (self)
+    local ctx = self:get()
+    local cacheKey = "ReceiveNewChatMsg"
+    local cacheItem = { ctx }
+    if PlayerHooks.Cache[cacheKey] then return end
+    PlayerHooks.Cache[cacheKey] = cacheItem
     Utils.Log("ReceiveNewChatMsg")
 end
 
-PlayerHooks.Methods.onSay = function (self) 
+PlayerHooks.Methods.onSay = function (self)
+    local ctx = self:get()
+    local cacheKey = "Say"
+    local cacheItem = { ctx }
+    if PlayerHooks.Cache[cacheKey] then return end
+    PlayerHooks.Cache[cacheKey] = cacheItem
     Utils.Log("Say")
 end
-PlayerHooks.Methods.onServerSay = function (self) 
+PlayerHooks.Methods.onServerSay = function (self)
+    local ctx = self:get()
+    local cacheKey = "ServerSay"
+    local cacheItem = { ctx }
+    if PlayerHooks.Cache[cacheKey] then return end
+    PlayerHooks.Cache[cacheKey] = cacheItem
     Utils.Log("ServerSay")
 end
-PlayerHooks.Methods.onServerTeamSay = function (self) 
+PlayerHooks.Methods.onServerTeamSay = function (self)
+    local ctx = self:get()
+    local cacheKey = "ServerTeamSay"
+    local cacheItem = { ctx }
+    if PlayerHooks.Cache[cacheKey] then return end
+    PlayerHooks.Cache[cacheKey] = cacheItem
     Utils.Log("ServerTeamSay")
 end
-PlayerHooks.Methods.onTeamSay = function (self) 
+PlayerHooks.Methods.onTeamSay = function (self)
+    local ctx = self:get()
+    local cacheKey = "TeamSay"
+    local cacheItem = { ctx }
+    if PlayerHooks.Cache[cacheKey] then return end
+    PlayerHooks.Cache[cacheKey] = cacheItem
     Utils.Log("TeamSay")
 end
 PlayerHooks.Methods.onServerAdmin = function (self, Cmd, Player)
+    local ctx = self:get()
     local Cmd = Cmd:get():ToString()
     local CommandParts = Cmd:split(" ")
+    local cacheKey = "ServerAdmin"
+    local cacheItem = { ctx, Cmd, CommandParts }
+    if PlayerHooks.Cache[cacheKey] then return end
+    PlayerHooks.Cache[cacheKey] = cacheItem
     Utils.Log("ServerAdmin Cmd: %s", dumpTable(Cmd))
     Utils.Log("ServerAdmin CommandParts: %s", dumpTable(CommandParts))
     -- Utils.Dump(self, "ServerAdmin.self")
@@ -59,66 +89,66 @@ end
 
 
 PlayerHooks.Methods.onPlayerJoined = function (self, NewPlayer) -- PlayerPostLogin 
+    local ctx = self:get()
     local player = NewPlayer:get()
     local cacheKey = "PlayerJoined"
-    local cacheItem = player
+    local cacheItem = { ctx, player }
     if PlayerHooks.Cache[cacheKey] then return end
     PlayerHooks.Cache[cacheKey] = cacheItem
-    -- local ctx = self:get()
     local state = Utils.GetPlayerState(player)
     Utils.Log("%s joined the server", Utils.GetPlayerString(state))
     Utils.SayChat(string.format("%s joined [%ims]", Utils.GetPlayerStr(state), state.Ping))
 end
 
-PlayerHooks.Methods.onPlayerJoinedTeam = function (self, JoiningPlayer, TeamNum) 
+PlayerHooks.Methods.onPlayerJoinedTeam = function (self, JoiningPlayer, TeamNum)
+    local ctx = self:get()
     local player = JoiningPlayer:get()
     local teamNum = TeamNum:get()
     local cacheKey = "PlayerJoinedTeam"
-    local cacheItem = {player, teamNum}
+    local cacheItem = {ctx, player, teamNum}
     if PlayerHooks.Cache[cacheKey] then return end
     PlayerHooks.Cache[cacheKey] = cacheItem
-    -- local ctx = self:get()
     local state = Utils.GetPlayerState(player)
     Utils.Log("%s joined team %s", Utils.GetPlayerString(state), Utils.GetTeamName(teamNum))
 end
 
-PlayerHooks.Methods.onPlayerSpawn = function (self, Player, NewPlayerPawn) 
+PlayerHooks.Methods.onPlayerSpawn = function (self, Player, NewPlayerPawn)
+    local ctx = self:get()
     local player = Player:get()
     local pawn = NewPlayerPawn:get()
     local cacheKey = "PlayerSpawn"
-    local cacheItem = {player, pawn}
+    local cacheItem = {ctx, player, pawn}
     if PlayerHooks.Cache[cacheKey] then return end
     PlayerHooks.Cache[cacheKey] = cacheItem
-    -- local ctx = self:get()
     local state = Utils.GetPlayerState(player)
     Utils.Log("%s spawned at %s", Utils.GetPlayerString(state), Utils.GetVectorString(pawn:K2_GetActorLocation()))
 end
 
 PlayerHooks.Methods.onPlayerKilled = function (self, Killer, Player) 
+    local ctx = self:get()
     local player = Player:get()
     local killer = Killer:get()
     local cacheKey = "PlayerKilled"
-    local cacheItem = {player, killer}
+    local cacheItem = {ctx, player, killer}
     if PlayerHooks.Cache[cacheKey] then return end
     PlayerHooks.Cache[cacheKey] = cacheItem
-    -- local ctx = self:get()
     local playerState = Utils.GetPlayerState(player)
     local killerState = Utils.GetPlayerState(killer)
     Utils.Log("%s killed by %s", Utils.GetPlayerString(playerState), Utils.GetPlayerString(killerState))
     Utils.SayChat(string.format("%s killed %s", Utils.GetPlayerStr(killerState), Utils.GetPlayerStr(playerState)))
 end
 
-PlayerHooks.Methods.onPlayerWounded = function (self, Player, DamageAmount, DamageType, InstigatedBy, DamageCauser) 
+PlayerHooks.Methods.onPlayerWounded = function (self, Player, DamageAmount, DamageType, InstigatedBy, DamageCauser)
+    local ctx = self:get()
     local player = Player:get()
     local damageAmount = DamageAmount:get()
     local damageType = DamageType:get()
     local instigatedBy = InstigatedBy:get()
     local damageCauser = DamageCauser:get()
     local cacheKey = "PlayerWounded"
-    local cacheItem = {player, damageAmount, damageType, instigatedBy, damageCauser}
+    local cacheItem = {ctx, player, damageAmount, damageType, instigatedBy, damageCauser}
     if PlayerHooks.Cache[cacheKey] then return end
     PlayerHooks.Cache[cacheKey] = cacheItem
-    -- local ctx = self:get()
     local playerState = Utils.GetPlayerState(player)
     local instigatorState = Utils.GetPlayerState(instigatedBy)
     local damageTypeStr = splitString(damageType:GetFullName()," ")[1]
@@ -128,32 +158,33 @@ PlayerHooks.Methods.onPlayerWounded = function (self, Player, DamageAmount, Dama
     )
 end
 
-PlayerHooks.Methods.onPlayerSuicide = function (self, Player) 
+PlayerHooks.Methods.onPlayerSuicide = function (self, Player)
+    local ctx = self:get()
     local player = Player:get()
     local cacheKey = "PlayerSuicide"
-    local cacheItem = player
+    local cacheItem = { ctx, player }
     if PlayerHooks.Cache[cacheKey] then return end
     PlayerHooks.Cache[cacheKey] = cacheItem
-    -- local ctx = self:get()
     local state = Utils.GetPlayerState(player)
     Utils.Log("%s suicided", Utils.GetPlayerString(state))
     Utils.SayChat(string.format("%s suicided", Utils.GetPlayerStr(state)))
 end
-PlayerHooks.Methods.onPlayerDied = function (self, Player) 
+PlayerHooks.Methods.onPlayerDied = function (self, Player)
+    local ctx = self:get()
     local player = Player:get()
     local cacheKey = "PlayerDied"
-    local cacheItem = player
+    local cacheItem = { ctx, player}
     if PlayerHooks.Cache[cacheKey] then return end
     PlayerHooks.Cache[cacheKey] = cacheItem
-    -- local ctx = self:get()
     local state = Utils.GetPlayerState(player)
     Utils.Log("%s died", Utils.GetPlayerString(state))
 end
 
-PlayerHooks.Methods.onPlayerPostLogout = function (self, ExitingPlayer) 
+PlayerHooks.Methods.onPlayerPostLogout = function (self, ExitingPlayer)
+    local ctx = self:get()
     local player = ExitingPlayer:get()
     local cacheKey = "PlayerPostLogout"
-    local cacheItem = player
+    local cacheItem = { ctx, player }
     if PlayerHooks.Cache[cacheKey] then return end
     PlayerHooks.Cache[cacheKey] = cacheItem
     local state = Utils.GetPlayerState(player)
